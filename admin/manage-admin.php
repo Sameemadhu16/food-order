9a7fbc4c26f1e58bd340bb4499c966edd93b9eb5
Sameemadhu16 <sameemadhu2003@gmail.com>
2024-09-22 18:59:43 +0530

    add admin ui buttons

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,13 +1,75 @@
 <?php include('partials/menu.php'); ?>
 
-        <!-- Main Content Section Starts -->
-        <div class="main-content">
-            <div class="wrapper">
-                <h1>Manage Admin</h1>
+<!-- Main Content Section Starts -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Admin</h1>
+        <br />
+        <!-- Button to add Admin -->
+        <a href="#" class="btn-primary">Add Admin</a>
+        <br /><br /><br />
+        <table class="tbl-full">
 
-               
-            </div>
-        </div>
-        <!-- Main Content Section Ends -->
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>4.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>5.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
+
+    </div>
+</div>
+<!-- Main Content Section Ends -->
 
 <?php include('partials/footer.php'); ?>
--- admin/manage-category.php
+++ admin/manage-category.php
@@ -4,7 +4,69 @@
         <div class="main-content">
             <div class="wrapper">
                 <h1>Manage Category</h1>
+                <br />
+        <!-- Button to add Admin -->
+        <a href="#" class="btn-primary">Add Category</a>
+        <br /><br /><br />
+        <table class="tbl-full">
 
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+
+            <tr>
+                <td>1.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>2.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>3.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sFameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>4.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+            <tr>
+                <td>5.</td>
+                <td>Sameera Madhuranga</td>
+                <td>sameera16</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+        </table>
                
             </div>
         </div>
